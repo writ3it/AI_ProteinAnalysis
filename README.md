394232a3860786917ca17895a984b99d61028aea
writ3it <42292507+writ3it@users.noreply.github.com>
2018-11-25 21:29:35 +0100

    update report

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # AI_ProteinAnalysis
 Basic analysis of ligands extracted from structures described in Protein Data Bank
+
+## Report preview
+[Click here to preview generated report](https://writ3it.github.io/AI_ProteinAnalysis/index.html)
--- src/build.r
+++ src/build.r
@@ -3,5 +3,10 @@ library(rmarkdown)
 
 d <-getwd()
 
-rmarkdown::render(paste(d,"/src/report.Rmd",sep=""),output_dir = paste(d,"/docs",sep=""),output_file = "index.html")
+rmarkdown::render(
+  paste(d,"/src/report.Rmd",sep=""),
+  output_dir = paste(d,"/docs",sep=""),
+  output_file = "index.html",
+  params=list( source_url="https://zenodo.org/record/1040778/files/all_summary.7z" )
+  )
 
