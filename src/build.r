b47616116637f5023bb497af023d3997190ff647
writ3it <42292507+writ3it@users.noreply.github.com>
2018-12-10 15:33:12 +0100

    new sample report

--- src/build.r
+++ src/build.r
@@ -7,7 +7,7 @@ rmarkdown::render(
   paste(d,"/src/report.Rmd",sep=""),
   output_dir = paste(d,"/docs",sep=""),
   output_file = "index.html",
-  params=list( source_url="https://zenodo.org/record/1040778/files/all_summary.7z",isSample=TRUE, csv_name="big_summary" )
+  params=list( source_url="https://zenodo.org/record/1040778/files/all_summary.7z",isSample=FALSE, csv_name="big_summary",cache=FALSE )
   )
 
 
